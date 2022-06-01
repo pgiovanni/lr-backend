182a7f336776ad537b832d41e3e8d3d4072a9315
Paul Giovanni <pgiovanni12342gmail.com>
2022-06-01 19:18:34 -0400

    fix: prisma

--- src/model/user/user.service.ts
+++ src/model/user/user.service.ts
@@ -2,23 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { User } from './user.type';
 import { UserCreateDto } from './dto/user-create.dto';
 import * as bcrypt from 'bcrypt';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '../../prisma';
 
 @Injectable()
 export class UserService {
-  prisma = new PrismaClient();
   async getUsers(): Promise<User[]> {
-    return await this.prisma.user.findMany();
+    return await prisma.user.findMany();
   }
 
   async createUser(data: UserCreateDto): Promise<User> {
     const saltOrRounds = 10;
     data.passwordHash = await bcrypt.hash(data.passwordHash, saltOrRounds);
-    return await this.prisma.user.create({ data });
+    return await prisma.user.create({ data });
   }
 
   async getUser(id: string): Promise<User> {
-    return await this.prisma.user.findUnique({
+    return await prisma.user.findUnique({
       where: { id },
     });
   }
